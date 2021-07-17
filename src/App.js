11233486b41eef133f10cb02f461f00dc66ce8dd
alexeykuz-sys <alexeykuz@gmail.com>
2021-07-17 12:50:00 +0100

    created card element

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Card from './components/Card';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        Bill Select Tip % 5% 10% 15% 25% 50% Custom Number of People Tip Amount / person Total / person Reset
-Challenge by Frontend Mentor. Coded by Your Name Here.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card/>  
     </div>
   );
 }
