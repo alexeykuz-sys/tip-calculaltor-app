bc559f8e9acf75647072f607481ed608defac652
Luciano Lima <ccll4ever1@Lucianos-MacBook-Pro.local>
2021-07-17 13:09:33 +0100

    Creating ImputForm component

---
+++ src/components/InputForm.js
@@ -0,0 +1,12 @@
+const InputForm = () => {
+    return (
+        <card>
+            <label htmlFor="tip">Tip</label>
+            <input type="text"/> 
+        </card> 
+        
+        
+    )
+}
+
+export default InputForm
