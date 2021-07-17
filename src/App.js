134611328bf1deec212f67334a34d37383323485
Luciano Lima <ccll4ever1@Lucianos-MacBook-Pro.local>
2021-07-17 13:48:03 +0100

    Removed duplicated css style

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import InputForm from './components/InputForm'
 
 function App() {
@@ -7,20 +6,6 @@ function App() {
 			<InputForm />
 		</card>
 	)
-=======
-import './App.css';
-import Card from './components/Card';
-import Button from './components/Button';
-
-function App() {
-  return (
-    <div className="App">
-      <Card>
-        <Button />
-      </Card>
-    </div>
-  );
->>>>>>> 3e993bd5b14f2ab046e09f6b44e8dfa2228eac14
 }
 
 export default App
--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -1,9 +1,8 @@
-<<<<<<< HEAD
 const InputForm = () => {
     return (
         <card>
             <label htmlFor="tip">Bill</label>
-            <input type="text" value="tip"/> 
+            <input type="text" value=""/> 
         </card> 
         
         
@@ -11,6 +10,3 @@ const InputForm = () => {
 }
 
 export default InputForm
-=======
-import './InputForm.css';
->>>>>>> 3e993bd5b14f2ab046e09f6b44e8dfa2228eac14
