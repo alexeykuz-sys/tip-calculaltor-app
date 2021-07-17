50f5b6b9d105954f5f4554a35b59ffb7bcc723e8
Mike Avgeros <mike.avgeros@gmail.com>
2021-07-17 12:53:47 +0100

    Created the components. Removed unecessary files.

---
+++ src/components/Button.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Button.css';
+
+const Button = (props) => {
+    const eventHandler = () => {
+        console.log("clicked")
+    }
+    return 
+}
+
---
+++ src/components/InputForm.js
@@ -0,0 +1,1 @@
+import './InputForm.css';
